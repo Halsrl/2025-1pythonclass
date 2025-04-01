003d654e8e9ba4ca7914390977fb7c97cbc323da
user <h01077396841@gmail.com>
2025-04-01 11:37:47 +0900

    다차원리스트와 조건문

--- p01pi.py
+++ p01pi.py
@@ -1,31 +1,46 @@
 # 오일러공식을 이용한 파이 근사값 구하기
 
 # 기본계산
-p=1
+
 
 
 
 n=1
-p = p * ((2*n + 1)**2 - 1)/ (2*n +1)**2
+result = 1
+
+while n <= 100:
+    a  =((2 * n + 1)**2 -1) / ((2*n + 1)**2)
+    result = result * a
+print(result*4)
 
 
+#기본계산
+n = 1
+p = 1
+p = p * ((2 * n + 1) ** 2 - 1) / (2 * n + 1) ** 2
 
 n=2
-p = p * ((2*n + 1)**2 - 1)/ (2*n +1)**2
+p = p*((2 * n + 1)**2 - 1)/ ( 2 *n +1)**2
+print(p*4)
 
 
-# print(p*4)
+
+pilist = [
+
+
+]
+#루프로 변환
+
 
 #루프로 변환
 p=1
-for n in range(1,100):
-     p = p * ((2 * n + 1) ** 2 - 1) / (2 * n + 1) ** 2
-
-     print(p*4)
-
+pilist = []
+for n in range(1,1000):
+    p = p *((2 * n + 1) ** 2 - 1) / (2 * n + 1) ** 2
+    #print(p*4, ',')
+    pilist.append(p*4)
 
 import matplotlib.pyplot as plt
-plt.plot([1,3,4])
+plt.plot(pilist)
 plt.show()
-
 
